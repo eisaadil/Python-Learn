956cd4e3a13debe217202d628ece774626a9f307
eisaadil <eisaadil@outlook.com>
2019-09-22 15:31:40 -0400

    Removed faulty code

--- BinarySearch.py
+++ BinarySearch.py
@@ -3,7 +3,7 @@ from random import randint
 
 class TestBinarySearch(unittest.TestCase):
 
-    def binary_search1(self, input_array, value): #Proper iterative solution
+    def binary_search_iterative(self, input_array, value): #Proper iterative solution
         start = 0
         end = len(input_array)-1
         while(start<=end):
@@ -15,23 +15,8 @@ class TestBinarySearch(unittest.TestCase):
             else:
                 return m
         return -1
-
-    def binary_search2(self, input_array, value): #My attempt (works but inefficient)
-        start = 0
-        end = len(input_array)-1
-        m = int((start+end)/2)
-        while(end-start >= 0):
-            if (value>input_array[m]):
-                start = m+1
-                m = int((start+end)/2)
-            elif (value<input_array[m]):
-                end = m-1
-                m = int((start+end)/2)
-            else:
-                return m
-        return -1
     
-    def binary_search_recursion(self, input_array, value, start=0, end=None):
+    def binary_search_recursive(self, input_array, value, start=0, end=None):
         if (end is None): end = len(input_array)
         if (end<start): return -1
         m = (start+end)//2
@@ -48,7 +33,7 @@ class TestBinarySearch(unittest.TestCase):
         for i in range(0,100):
             with self.subTest(i=i):
                 r = randint(0,len(s)-1)
-                self.assertEqual(self.binary_search2(s,s[r]), r)
+                self.assertEqual(self.binary_search_recursive(s,s[r]), r)
 
 if __name__ == '__main__':
     unittest.main()
